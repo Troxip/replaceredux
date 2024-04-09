b4b34784aaffeb103e3e08fc5b0ace591d642fa4
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-09 21:34:26 +0200

    Start

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
 
-import Navigation from './components/Nav/Navigation';
-import ProductsPage from './containers/Products';
-import FavoritesPage from './containers/Favorites';
+const App = () => {
+  const test = "test";
+  console.log(test);
 
-const App = props => {
   return (
-    <React.Fragment>
-      <Navigation />
-      <main>
-        <Route path="/" component={ProductsPage} exact />
-        <Route path="/favorites" component={FavoritesPage} />
-      </main>
-    </React.Fragment>
+    <>
+      <div>Hello</div>
+    </>
   );
 };
 
